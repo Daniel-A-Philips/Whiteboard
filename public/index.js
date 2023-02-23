04b8d686b7b99249960bf0096959ae252d1326a9
Teague Millette <tfm47@drexel.edu>
2023-02-23 10:15:44 -0500

    basic frontend setup for functionality

---
+++ public/index.js
@@ -0,0 +1,33 @@
+function dateFromBBString(str) {
+	return new Date(Date.parse(str))
+}
+
+function applyDataToPage(data) {
+	applyDataToCalendar(data)
+	applyDataToSidebar(data)
+	applyDataToAssignments(data)
+}
+
+function applyDataToCalendar(data) {
+	for (key of Object.keys(data)) {
+		if (!key.startsWith("_blackboard.platform.gradebook2.GradableItem")) {
+			continue
+		}
+	}
+}
+
+function applyDataToSidebar(data) {
+	for (key of Object.keys(data)) {
+		if (!key.startsWith("_blackboard.platform.gradebook2.GradableItem")) {
+			continue
+		}
+	}
+}
+
+function applyDataToAssignments(data) {
+	for (key of Object.keys(data)) {
+		if (!key.startsWith("_blackboard.platform.gradebook2.GradableItem")) {
+			continue
+		}
+	}
+}
