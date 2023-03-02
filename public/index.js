e2ee38958409b5c379a34fd12095c4776d6e9460
Joshua Eddy <je578@drexel.edu>
2023-03-02 09:10:11 -0500

    Added updated calendar dates

--- public/index.js
+++ public/index.js
@@ -33,6 +33,41 @@ function applyDataToAssignments(data) {
 	}
 }
 
+
+//add auto dates to the calendar instead of manuel input
+
+function updateCalendar() {
+	const daysOfWeek = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
+	const today = new Date();
+	const todayIndex = today.getDay();
+  
+	for (let i = 1; i < 8; i++) {
+	  const date = new Date(today);
+	  date.setDate(date.getDate() + i - todayIndex);
+	  const dayOfWeek = daysOfWeek[date.getDay()];
+	  const dayOfMonth = date.getDate();
+	  const elementId = "day" + i;
+	  const element = document.getElementById(elementId);
+	  element.textContent = `${dayOfWeek}, ${dayOfMonth}`;
+		//helps find current date for bold
+	  if(i === todayIndex + 0) {
+		element.classList.add("current-date");
+	  }
+	}
+  }
+  //runs every 7 days
+  document.addEventListener("DOMContentLoaded", function() {
+	updateCalendar();
+	setInterval(updateCalendar, 7 * 24 * 60 * 60 * 1000);
+  });
+  
+  
+
+
+
+
+
+
   
   
 
